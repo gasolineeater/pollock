672858ea697c4ee5169a7fde6cdf91f3ffb66a2b
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-22 20:30:15 +0200

    Add: kind of percent way of how much pixels we are puting in the window

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "MLX42/MLX42_Int.h"
 
 #define WIDTH 800
-#define HEIGHT 800
+#define HEIGHT 500
 
 int	main(void)
 {
@@ -11,13 +11,13 @@ int	main(void)
 	mlx_t		*mlx;
 	mlx_image_t	*img;
 
-	y = 50;
+	y = HEIGHT * 0.1;
 	mlx = mlx_init(WIDTH, HEIGHT, "test", 1);
 	img = mlx_new_image(mlx, WIDTH, HEIGHT);
-	while (y < HEIGHT - 50)
+	while (y < HEIGHT * 0.9)
 	{
-		x = 50;
-		while (x < WIDTH - 50)
+		x = WIDTH * 0.1;
+		while (x < WIDTH * 0.9)
 		{
 			mlx_put_pixel(img, x, y, 0xFF0000FF);
 			x++;
