52a6364b432936cec5f2a0f765c72091ea784e33
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-22 20:09:25 +0200

    Add: Skipped a step of implementation when we just puted a pixel on the image but we just remove the iterator and the while loop and boom.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,23 @@
 #include "MLX42/MLX42.h"
 #include "MLX42/MLX42_Int.h"
 
+#define WIDTH 800
+#define HEIGHT 800
+
 int	main(void)
 {
-	void	*connection;
-	connection = mlx_init(800, 800, "test", 1);
-	mlx_loop(connection);
+	int			i;
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+
+	i = 0;
+	mlx = mlx_init(WIDTH, HEIGHT, "test", 1);
+	img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	while (i <= WIDTH/2 - 1)
+	{
+		mlx_put_pixel(img, WIDTH/2 + i, HEIGHT/2 + i, 0xFF0000FF);
+		i++;
+	}
+	mlx_image_to_window(mlx, img, 0, 0);
+	mlx_loop(mlx);
 }
